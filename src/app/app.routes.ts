aba4420764df4bc00862239c7bfd0db922a2628c
GUSTAVO CONCEIÇÃO PERUCI DE ASSIS <gustavo.assis@catolicasc.edu.br>
2024-04-30 20:58:47 -0300

    feat: melhorias na tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: SignUpComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Página inicial redireciona para login
 ];
 
